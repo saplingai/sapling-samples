906cf0d83e17014484dd73368c61f850b6ab4261
James Huang <hng.jms@gmail.com>
2023-07-10 09:52:13 -0700

    vue demo

---
+++ javascript/vue-app/README.md
@@ -0,0 +1,21 @@
+# Sapling SDK: Example Vue App
+
+This app is an example of integrating Sapling into a Vue application.
+Sapling is declared in `package.json` installed as a dependancy through npm.
+
+
+## Installation
+
+- `npm install`
+- Register for an account at [https://sapling.ai](https://sapling.ai/user/register), then sign in.
+- Generate a development API key in your dashboard.
+- Replace `<YOUR_API_KEY>` in `src/App.vue` with your Sapling API key.
+
+
+## Running
+
+Run `npm run dev`
+
+This runs the app in the development mode.
+
+Then visit [http://http://localhost:5173/](http://http://localhost:5173/) to view the app in your browser.
---
+++ javascript/vue-app/src/App.vue
@@ -0,0 +1,36 @@
+<script setup>
+import { Sapling } from "@saplingai/sapling-js/observer";
+import { ref, onMounted } from 'vue'
+
+const editorRef = ref(null);
+
+onMounted(() => {
+  console.log('onMounted');
+  Sapling.init({
+    key: '<API_KEY>',
+    endpointHostname: 'https://api.sapling.ai',
+    editPathname: '/api/v1/edits',
+    statusBadge: true,
+    mode: 'dev',
+  });
+
+  console.log('editorRef.value:', editorRef.value);
+  Sapling.observe(editorRef.value);
+})
+</script>
+
+<template>
+  <div id="editor" ref="editorRef" sapling-ignore="true" contenteditable>
+    Lets get started.
+  </div>
+</template>
+
+<style scoped>
+#editor{
+  height: 200px;
+  width:  400px;
+  padding: 10px;
+  margin: 10px;
+  border: 1px solid;
+}
+</style>
